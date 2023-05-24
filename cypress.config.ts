ddbe73200beec6b66fbd0b52b9e66c99c501c929
jun sheng <jsheng@juns-home-mbp.lan>
2023-05-24 22:56:27 +1000

    add first test on 72954

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,20 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress'
 
 export default defineConfig({
+  chromeWebSecurity: false,
+
+  pageLoadTimeout: 60000,
+  screenshotsFolder: "test-reports",
+  video: true,
+  videosFolder: "test-reports",
+  videoUploadOnPasses: false,
+  trashAssetsBeforeRuns: true,
+
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    configFile: 'reporter-config.json',
+  },
+  
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
