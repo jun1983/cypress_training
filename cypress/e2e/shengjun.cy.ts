a6bb6be145c5431e85cafb16017182bb2b511c2d
jun sheng <jsheng@juns-home-mbp.lan>
2023-05-26 12:13:35 +1000

    add new test to 72954

---
+++ cypress/e2e/shengjun.cy.ts
@@ -0,0 +1,27 @@
+describe('obstacle shengjun', () => {
+    xit('72954', () => {
+      cy.visit('https://obstaclecourse.tricentis.com/Obstacles/72954')
+      cy.contains("Click Me 2x").click()
+      cy.contains("Click me once more").click()
+      cy.contains("Good job!")
+    })
+
+    it('81121', () => {
+      cy.visit('https://obstaclecourse.tricentis.com/Obstacles/81121')
+      
+      let clickButton = () => {
+        cy.get("#button").then(($element)=>{
+            if($element.text() == 'Enough'){
+              $element.click()
+              return
+            }
+            $element.click()
+            clickButton()
+          })
+        }
+      
+      clickButton()
+      
+      cy.contains("Good job!")
+    })
+  })
