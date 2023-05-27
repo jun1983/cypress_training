fc11cd3d07be1bae4ca4eaeab0629bc5ef6b3d0c
Jackywang96 <jackywangmel96@gmail.com>
2023-05-28 02:36:20 +1000

    Update jackwang.cy.ts

---
+++ cypress/e2e/jackwang.cy.ts
@@ -0,0 +1,61 @@
+import * as dayjs from "dayjs"
+
+it('19875', () =>{
+    cy.visit('https://obstaclecourse.tricentis.com/Obstacles/19875')
+    const text : string =dayjs().add(1,'day')
+        .format('DD.MM.YYYY')
+
+    cy.get('#datefield').type(text)
+
+    cy.contains("Good job")
+
+})
+
+//***************************************************************************
+it('41040', () =>{
+    cy.visit('https://obstaclecourse.tricentis.com/Obstacles/41040')
+
+    cy.get('#buttontoclick').trigger('click')
+
+    cy.contains("Good job")
+
+})
+
+//***************************************************************************
+it('73588',()=> {
+    cy.visit('https://obstaclecourse.tricentis.com/Obstacles/73588/')
+    cy.get('#clickme').should('exist')
+    cy.get('#clickme').click()
+
+    cy.get('#randomtext').should('not.have.value', '')
+
+    cy.get('#randomtext').invoke('val').then((val) => {
+        cy.get('#selectlink').children().each(($el) => {
+                if ($el.text() === val) {
+                    cy.get('#selectlink').select($el.text())
+                }
+            }
+        )
+        cy.get('#submitanswer').click()
+    })
+    cy.contains("Good job")
+})
+
+
+//***************************************************************************
+it('81012', ()=> {
+        cy.visit('https://obstaclecourse.tricentis.com/Obstacles/81012')
+        cy.get('#alerttext').invoke('text').then((text) => {
+            const regex = /\$([\d.,]+)/
+            const matches = text.match(regex)
+            const amount = matches ? matches[1] : null
+
+            if (amount === null){
+                throw new Error('Amount is null')
+            }
+            cy.get('#totalamountText').type(amount)
+        })
+    cy.contains("Good job")
+})
+
+
