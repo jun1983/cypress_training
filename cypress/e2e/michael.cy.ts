8326ac100dd13d04685d625621771881f28a6df5
“kaka5277” <254140626@qq.com>
2023-05-28 18:18:56 +1000

    add 'Good job!'for every test case and remove semicolon

--- cypress/e2e/michael.cy.ts
+++ cypress/e2e/michael.cy.ts
@@ -1,18 +1,21 @@
 describe('obstacle practice', () => {
-    it('click twice', () => {
-        cy.visit('https://obstaclecourse.tricentis.com/Obstacles/72954');
-        cy.contains('a','Click Me 2x').click()
-        cy.contains('a','Click me once more').click()
+    it('72954-click twice', () => {
+        cy.visit('https://obstaclecourse.tricentis.com/Obstacles/72954')
+        cy.contains('a', 'Click Me 2x').click()
+        cy.contains('a', 'Click me once more').click()
+        cy.contains('Good job!')
     });
 
-    it('41041', () => {
+    it('41041-input', () => {
         cy.visit('https://obstaclecourse.tricentis.com/Obstacles/41041')
         cy.get('input[name="resulttext"]')
             .type('{Click}', { parseSpecialCharSequences: false })
+        cy.contains('Good job!')
     });
 
-    it('drag', () => {
+    it('60469-drag', () => {
         cy.visit('https://obstaclecourse.tricentis.com/Obstacles/60469')
-        cy.get('#toscabot').dragTo('#to');
+        cy.get('#toscabot').dragTo('#to')
+        cy.contains('Good job!')
     })
 })
