bf4958bfe79cbf180fdc00db1c15e5698215c782
Jackywang96 <jackywangmel96@gmail.com>
2023-05-30 00:17:02 +1000

    Update jackwang.cy.ts

--- cypress/e2e/jackwang.cy.ts
+++ cypress/e2e/jackwang.cy.ts
@@ -11,48 +11,34 @@ it('19875', () =>{
 //***************************************************************************
 it('41040', () =>{
     cy.visit('https://obstaclecourse.tricentis.com/Obstacles/41040')
-
     cy.get('#buttontoclick').trigger('click')
-
-    cy.contains("Good job")
-
+    cy.contains("Good job!")
 })
 
 //***************************************************************************
-it('73588',()=> {
+it('73588', ()=> {
     cy.visit('https://obstaclecourse.tricentis.com/Obstacles/73588/')
-    cy.get('#clickme').should('exist')
     cy.get('#clickme').click()
-
     cy.get('#randomtext').should('not.have.value', '')
-
-    cy.get('#randomtext').invoke('val').then((val) => {
-        cy.get('#selectlink').children().each(($el) => {
-                if ($el.text() === val) {
-                    cy.get('#selectlink').select($el.text())
-                }
-            }
-        )
-        cy.get('#submitanswer').click()
+    cy.get('#randomtext').invoke('val').then((val)=>{
+        cy.get('#selectlink').select(val)
     })
-    cy.contains("Good job")
+    cy.get('#submitanswer').click()
+    cy.contains("Good job!")
 })
-
 
 //***************************************************************************
 it('81012', ()=> {
-        cy.visit('https://obstaclecourse.tricentis.com/Obstacles/81012')
-        cy.get('#alerttext').invoke('text').then((text) => {
-            const regex = /\$([\d.,]+)/
-            const matches = text.match(regex)
-            const amount = matches ? matches[1] : null
+    cy.visit('https://obstaclecourse.tricentis.com/Obstacles/81012')
+    cy.get('#alerttext').invoke('text').then((text) => {
+        const regex = /\$([\d.,]+)/
+        const matches = text.match(regex)
+        const amount = matches ? matches[1] : null
 
-            if (amount === null){
-                throw new Error('Amount is null')
-            }
-            cy.get('#totalamountText').type(amount)
+        if (!matches){
+            throw new Error('Amount is null')
+        }
+        cy.get('#totalamountText').type(amount)
         })
-    cy.contains("Good job")
+    cy.contains("Good job!")
 })
-
-
