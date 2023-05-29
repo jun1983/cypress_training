882fcb70a9eee5f13075239b215e2257cb32f214
“kaka5277” <254140626@qq.com>
2023-05-30 08:19:32 +1000

    add 3 example in this end file

--- cypress/e2e/michael.cy.ts
+++ cypress/e2e/michael.cy.ts
@@ -4,41 +4,60 @@ describe('obstacle practice', () => {
         cy.contains('a', 'Click Me 2x').click()
         cy.contains('a', 'Click me once more').click()
         cy.contains('Good job!')
-    });
-
+    })
     it('41041-input', () => {
         cy.visit('https://obstaclecourse.tricentis.com/Obstacles/41041')
         cy.get('input[name="resulttext"]')
             .type('{Click}', { parseSpecialCharSequences: false })
         cy.contains('Good job!')
-    });
-
+    })
     it('60469-drag', () => {
         cy.visit('https://obstaclecourse.tricentis.com/Obstacles/60469')
         cy.get('#toscabot').dragTo('#to')
         cy.contains('Good job!')
-    });
-
+    })
     it('41038', () => {
         cy.visit('https://obstaclecourse.tricentis.com/Obstacles/41038')
-        cy.contains('Click into my right half').click('right', { force: true })
+        cy.contains('Click into my right half').click('right')
         cy.contains('Good job!')
-    });
-
-    it('41037', () => {
-        cy.visit('https://obstaclecourse.tricentis.com/Obstacles/41037')
-        cy.get('tbody > :nth-child(3) > :nth-child(5)').invoke('text').then((text) => {
-            cy.get('input[name=resulttext]')
-                .type(text)
-            cy.contains('Good job!')
-
-        })
-    });
-
+    })
     it('22505', () => {
         cy.visit('https://obstaclecourse.tricentis.com/Obstacles/22505')
         cy.contains('Click me!').click()
         cy.contains('Good job!')
-    });
-    
+    })
+    it('12952', () => {
+        cy.visit('https://obstaclecourse.tricentis.com/Obstacles/12952')
+        cy.get('a[onclick^="obstacleCompleted"]').click()
+        cy.contains('Good job!')
+    })
+    it('81012', () => {
+        cy.visit('https://obstaclecourse.tricentis.com/Obstacles/81012/retry')
+        cy.contains('We charged the total amount of')
+            .invoke('text')
+            .then(text => {
+                const regex = /\d+(\.\d+)?/;
+                const match = text.match(regex);
+                const number = match ? parseFloat(match[0]) : null;
+                cy.get('#totalamountText').type(number).type('{enter}')
+                cy.contains('Good job!')
+            })
+    })
+    it.only('78264', () => {
+        cy.visit('https://obstaclecourse.tricentis.com/Obstacles/78264')
+        let number1, number2, result;
+        cy.get('#no1')
+            .invoke('text')
+            .then(text => {
+                number1 = parseFloat(text);
+                cy.get('#no2')
+                    .invoke('text')
+                    .then(text => {
+                        number2 = parseFloat(text);
+                        result = number1 + number2;
+                        cy.get("#result").type(result)
+                    })
+            })
+        cy.contains('Good job!')
+    })
 })
