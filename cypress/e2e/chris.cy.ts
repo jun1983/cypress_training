0d3fcd5333e6b510c5a40a39a1643bf05cd3a580
ChrisN <nnllqq123@hotmail.com>
2023-06-21 18:09:35 +1000

    Process the xml content without downloading file

--- cypress/e2e/chris.cy.ts
+++ cypress/e2e/chris.cy.ts
@@ -1,39 +1,34 @@
-const xml2js = require('xml2js')
+import { parseString } from 'xml2js';
 
 describe("", () => {
-    it("Obstacle #72946", () => {
-        cy.visit('https://obstaclecourse.tricentis.com/Obstacles/72946');
-        // download the xml file
-        cy.request({
-            url: '/Obstacles/GetObstacleFile?obstacleId=72946&filename=Catalog.xml',
-            method: 'GET',
-            encoding: 'binary',
-          }).then((response) => {
-            const fileName = 'Catalog.xml';
-            cy.writeFile(fileName, response.body, 'binary');
+  it("Obstacle #72946", () => {
+    cy.visit('https://obstaclecourse.tricentis.com/Obstacles/72946');
+    // Get the body text of the xml file
+    cy.request({
+      url: '/Obstacles/GetObstacleFile?obstacleId=72946&filename=Catalog.xml',
+      method: 'GET',
+      encoding: 'binary',
+    })
+    // Parse to xml and convert to json
+    .its('body').then((xml) => {
+      parseString(xml, (err, result) => {
+        if (err) {
+          console.error('Error converting XML to JSON:', err);
+        } else {
+          const jsonContent = JSON.stringify(result);
+          const data = JSON.parse(jsonContent);
+    // Iterate through all users and put Sue's number as input
+          data.catalog.number.forEach((item) => {
+            if (item.$.id === "Sue") {
+              const prefix = item.prefix[0];
+              const number = item.number[0];
+              const fullNumber = prefix.concat(number);
+              cy.get('#NumberSue').type(fullNumber);
+              cy.contains("Good job!")
+            }
           });
-        // convert xml to json
-        cy.readFile('Catalog.xml').then((xmlContent) => {
-            const parser = new xml2js.Parser();
-            parser.parseString(xmlContent, (err, result) => {
-              if (err) {
-                throw new Error('Failed to parse XML');
-              }
-        // read json file and extract the numbers
-              const jsonContent = JSON.stringify(result);
-              const data = JSON.parse(jsonContent)
-              const catalog = data.catalog;
-                catalog.number.forEach((item) => {
-                  if (item.$.id === "Sue") {
-                    const prefix = item.prefix[0];
-                    const number = item.number[0];
-                    const fullNumber = prefix.concat(number);
-                    cy.get('#NumberSue').type(fullNumber);
-                    cy.contains("Good job!")
-                  }
-                });
-            });
-          });
-
-})
+        }
+      })
+    })
+  })
 })
